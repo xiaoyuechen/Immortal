fc09f687c58fbb191f7b3c92ca3e49d3f9393be6
Xiaoyue Chen <xiaoyue_chen@outlook.com>
2018-10-19 13:13:37 +0200

    ExampleProject

---
+++ Source/Immortal/ImmortalGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2018 Epic Games, Inc. All Rights Reserved.
+
+#include "ImmortalGameMode.h"
+#include "ImmortalCharacter.h"
+
+AImmortalGameMode::AImmortalGameMode()
+{
+	// Set default pawn class to our character
+	DefaultPawnClass = AImmortalCharacter::StaticClass();	
+}
